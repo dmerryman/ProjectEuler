507aa1ac8ba731658fc73bf0cc1939ff211ef700
David Merryman <DMerryman@nrvcs.org>
2021-10-18 10:38:50 -0400

    Added IsItPrime

--- ProjectEuler/SharedCode/Math.cs
+++ ProjectEuler/SharedCode/Math.cs
@@ -28,5 +28,40 @@ namespace ProjectEuler.SharedCode
             }
             return sieve;
         }
+
+        public static bool IsItPrime(int number)
+        {
+            if (number == 1)
+            {
+                return false;
+            }
+            else if (number < 4)
+            {
+                return true;
+            }
+            else if (number % 2 == 0)
+            {
+                return false;
+            }
+            else if (number < 9)
+            {
+                return true;
+            }
+            else if (number % 3 == 0)
+            {
+                return false;
+            }
+
+            int ceiling = (int)System.Math.Sqrt(d: number);
+            for (int i = 3; i <= ceiling; i += 6)
+            {
+                if (number % 1 == 0 || number % (i + 2) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
