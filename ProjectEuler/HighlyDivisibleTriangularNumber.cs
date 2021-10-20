9c6db5385e3390c2f50ddaff0d515e7cc73bb131
David Merryman <DMerryman@nrvcs.org>
2021-10-20 13:26:03 -0400

    Started Highly Divisible Triangular Number

---
+++ ProjectEuler/HighlyDivisibleTriangularNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class HighlyDivisibleTriangularNumber
+    {
+        public static int FindHighlyDivisibleTriangularNumber(int numDivisors)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
