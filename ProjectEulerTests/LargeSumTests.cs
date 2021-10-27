618ca3bdc9099d852ae9a086e0e2304ecbabfb47
David Merryman <DMerryman@nrvcs.org>
2021-10-27 14:03:40 -0400

    Solved. But I'd like it if it could take a parameter and return the first N digits instead of just assuming 10.

---
+++ ProjectEulerTests/LargeSumTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ProjectEuler;
+
+namespace ProjectEulerTests
+{
+    [TestClass]
+    public class LargeSumTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual("5537376230", ProjectEuler.LargeSum.FindLargeSum());
+        }
+    }
+}
