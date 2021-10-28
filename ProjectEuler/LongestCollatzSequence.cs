e9ed085876c8cca14601f364d1281c748a315430
David Merryman <DMerryman@nrvcs.org>
2021-10-28 16:00:37 -0400

    Started GetNumberOfStepsInCollatzSequence

---
+++ ProjectEuler/LongestCollatzSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class LongestCollatzSequence
+    {
+        public static int FindLongestCollatzSequence(int ceiling)
+        {
+            // From 1 to ceiling, go through the number of steps in each Collatz sequence, and get the number of steps involved.
+            // For each number of steps, check if it exceeds the value of the previous maximum, and if so, set the maximum to be that value.
+            // Return the maximum value
+            throw new NotImplementedException();
+        }
+
+        private static int GetNumberOfStepsInCollatzSequence(int startingNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/LongestCollatzSequenceTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ProjectEuler;
+
+namespace ProjectEulerTests
+{
+    [TestClass]
+    public class LongestCollatzSequenceTests
+    {
+
+        [TestMethod]
+        public void TestLarge()
+        {
+            Assert.AreEqual(837799, LongestCollatzSequence.FindLongestCollatzSequence(ceiling: 1000000));
+        }
+    }
+}
