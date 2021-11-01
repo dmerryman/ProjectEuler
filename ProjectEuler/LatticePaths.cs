f0cf2fa44ed73e69d9e9e2bbd0d3092e81aeb772
David Merryman <DMerryman@nrvcs.org>
2021-11-01 16:29:04 -0400

    Started FindLatticePaths

---
+++ ProjectEuler/LatticePaths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class LatticePaths
+    {
+        public static int FindLatticePaths(int squareSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
