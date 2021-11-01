3b2bce9a29634d7afb6eb9177cf46b224b39763d
David Merryman <DMerryman@nrvcs.org>
2021-11-01 13:44:41 -0400

    Fixed a bug

--- ProjectEuler/LongestCollatzSequence.cs
+++ ProjectEuler/LongestCollatzSequence.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,17 +9,75 @@ namespace ProjectEuler
 {
     public static class LongestCollatzSequence
     {
-        public static int FindLongestCollatzSequence(int ceiling)
+        public static int FindLongestCollatzSequenceSlow(int ceiling)
         {
             // From 1 to ceiling, go through the number of steps in each Collatz sequence, and get the number of steps involved.
             // For each number of steps, check if it exceeds the value of the previous maximum, and if so, set the maximum to be that value.
             // Return the maximum value
-            throw new NotImplementedException();
+            int numberToCheck = 2;
+            int numberOfSteps = GetNumberOfStepsInCollatzSequenceSlow(startingNumber: numberToCheck);
+            int maximumNumberOfSteps = numberOfSteps;
+            int valueWithMaximumNumberOfSteps = numberToCheck;
+            while (numberToCheck <= ceiling)
+            {
+                numberToCheck++;
+                numberOfSteps = GetNumberOfStepsInCollatzSequenceSlow(startingNumber: numberToCheck);
+                if (numberOfSteps > maximumNumberOfSteps)
+                {
+                    maximumNumberOfSteps = numberOfSteps;
+                    valueWithMaximumNumberOfSteps = numberToCheck;
+                }
+                //Debug.WriteLine("{0} had {1} steps. Maximum is {2} with {3}", numberToCheck, numberOfSteps, valueWithMaximumNumberOfSteps, maximumNumberOfSteps);
+            }
+
+            return valueWithMaximumNumberOfSteps;
         }
 
-        private static int GetNumberOfStepsInCollatzSequence(int startingNumber)
+        public static int FindLongestCollatzSequenceFaster(int ceiling)
         {
-            throw new NotImplementedException();
+            // From 1 to ceiling, go through the number of steps in each Collatz sequence, and get the number of steps involved.
+            // For each number of steps, check if it exceeds the value of the previous maximum, and if so, set the maximum to be that value.
+            // Return the maximum value
+            int numberToCheck = ceiling / 2;
+            int numberOfSteps = GetNumberOfStepsInCollatzSequenceSlow(startingNumber: numberToCheck);
+            int maximumNumberOfSteps = numberOfSteps;
+            int valueWithMaximumNumberOfSteps = numberToCheck;
+            while (numberToCheck <= ceiling)
+            {
+                numberToCheck++;
+                numberOfSteps = GetNumberOfStepsInCollatzSequenceSlow(startingNumber: numberToCheck);
+                if (numberOfSteps > maximumNumberOfSteps)
+                {
+                    maximumNumberOfSteps = numberOfSteps;
+                    valueWithMaximumNumberOfSteps = numberToCheck;
+                }
+                //Debug.WriteLine("{0} had {1} steps. Maximum is {2} with {3}", numberToCheck, numberOfSteps, valueWithMaximumNumberOfSteps, maximumNumberOfSteps);
+            }
+
+            return valueWithMaximumNumberOfSteps;
+        }
+
+        private static int GetNumberOfStepsInCollatzSequenceSlow(long startingNumber)
+        {
+            // numberOfSteps = 0
+            // While startingNumber > 1
+            // if startingNumber is even, divide by two
+            // if startingNumber is odd, multiply by three
+            // Add one to startingNumber.
+            int numberOfSteps = 0;
+            while (startingNumber > 1)
+            {
+                if (startingNumber % 2 == 0)
+                {
+                    startingNumber /= 2;
+                }
+                else
+                {
+                    startingNumber = (startingNumber * 3) + 1;
+                }
+                numberOfSteps++;
+            }
+            return numberOfSteps;
         }
     }
 }
--- ProjectEulerTests/LongestCollatzSequenceTests.cs
+++ ProjectEulerTests/LongestCollatzSequenceTests.cs
@@ -11,7 +11,13 @@ namespace ProjectEulerTests
         [TestMethod]
         public void TestLarge()
         {
-            Assert.AreEqual(837799, LongestCollatzSequence.FindLongestCollatzSequence(ceiling: 1000000));
+            Assert.AreEqual(837799, LongestCollatzSequence.FindLongestCollatzSequenceSlow(ceiling: 1000000));
+        }
+
+        [TestMethod]
+        public void TestLargeFaster()
+        {
+            Assert.AreEqual(837799, LongestCollatzSequence.FindLongestCollatzSequenceFaster(ceiling: 1000000));
         }
     }
 }
