6216cab8b979f679ba375916de4d11b80593e24e
David Merryman <DMerryman@nrvcs.org>
2021-11-03 11:28:45 -0400

    Started FindPowerDigitSum and set up a unit test

---
+++ ProjectEuler/PowerDigitSum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class PowerDigitSum
+    {
+        public static int FindPowerDigitSum(int exponent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/PowerDigitSumTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests
+{
+    [TestClass]
+    public class PowerDigitSumTests
+    {
+        [TestMethod] public void TestSmall()
+        {
+            Assert.AreEqual(26, ProjectEuler.PowerDigitSum.FindPowerDigitSum(exponent: 15));
+        }
+    }
+}
