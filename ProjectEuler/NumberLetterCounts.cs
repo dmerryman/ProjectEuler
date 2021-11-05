7867d1807dd569868abce8c8f8682c8299e93d86
David Merryman <DMerryman@nrvcs.org>
2021-11-05 11:41:46 -0400

    Solved, but the readability could probably be improved a bit

---
+++ ProjectEuler/NumberLetterCounts.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class NumberLetterCounts
+    {
+        private static string connectorWord = "and";
+        private static string hundredWord = "hundred";
+        private static string thousandWord = "thousand";
+
+        private static string[] ones = new string[]
+            { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        private static string[] teens =
+        {
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+        };
+
+        private static string[] tens =
+        {
+            "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+        };
+
+        public static int FindNumberLetterCounts(int ceiling)
+        {
+            // loop through N from 1 to ceiling.
+            // Set a variable to track the total sum of the letter counts
+            // for each N, calculate the sum of the letter counts, and add it to the total sum.
+            int totalNumberOfLetters = 0;
+            for (int i = 1; i <= ceiling; i++)
+            {
+                totalNumberOfLetters += GetNumberOfLettersInValue(value: i);
+            }
+
+            return totalNumberOfLetters;
+            throw new NotImplementedException();
+        }
+
+        public static int GetNumberOfLettersInValue(int value)
+        {
+            // Cases to consider
+            // even thousand
+            // even hundred
+            // even tens
+            // last is a teen
+            // last single digit
+            int numberOfLetters = 0;
+            if (value > 999 && value <= 1000)
+            {
+                numberOfLetters += ones[0].Length + thousandWord.Length;
+            }
+
+            if (value > 99)
+            {
+                if (value % 1000 != 0)
+                {
+                    numberOfLetters += ones[(value / 100) - 1].Length;
+                    numberOfLetters += hundredWord.Length;
+                    if (value % 100 != 0)
+                    {
+                        numberOfLetters += connectorWord.Length;
+                        Debug.WriteLine("Connector word added for {0}", value);
+                    }
+                }
+            }
+
+            if (value % 100 > 19 || value % 100 == 10) // add || value % 100 == 10?
+            {
+                if (value % 100 != 0)
+                {
+                    int thisTens = value % 100 / 10;
+                    numberOfLetters += tens[thisTens - 1].Length;
+                }
+            }
+
+            if (value % 100 > 10 && value % 100 < 20)
+            {
+                numberOfLetters += teens[(value % 10) - 1].Length;
+            }
+
+            else if (value % 10 != 0)
+            {
+                int thisOnes = value % 10;
+                numberOfLetters += ones[thisOnes - 1].Length;
+            }
+            Debug.WriteLine("{0} has {1} letters.", value, numberOfLetters);
+            return numberOfLetters;
+        }
+
+        
+
+    }
+}
---
+++ ProjectEulerTests/NumberLetterCountsTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests
+{
+    [TestClass]
+    public class NumberLetterCountsTest
+    {
+        [TestMethod]
+        public void TestSmall()
+        {
+            Assert.AreEqual(19, ProjectEuler.NumberLetterCounts.FindNumberLetterCounts(ceiling: 5));
+        }
+
+        [TestMethod]
+        public void TestLarge()
+        {
+            Assert.AreEqual(21124, ProjectEuler.NumberLetterCounts.FindNumberLetterCounts(ceiling: 1000));
+        }
+
+        [TestMethod]
+        public void TestSpecificNumbers()
+        {
+            Assert.AreEqual(11, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 1000));
+            Assert.AreEqual(11, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 500));
+            Assert.AreEqual(19, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 540));
+            Assert.AreEqual(22, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 542));
+            Assert.AreEqual(22, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 513));
+            Assert.AreEqual(17, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 502));
+            Assert.AreEqual(16, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 101));
+            Assert.AreEqual(5, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 40));
+            Assert.AreEqual(9, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 45));
+            Assert.AreEqual(8, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 13));
+            Assert.AreEqual(3, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 10));
+            Assert.AreEqual(5, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 7));
+            Assert.AreEqual(16, ProjectEuler.NumberLetterCounts.GetNumberOfLettersInValue(value: 110));
+        }
+    }
+}
