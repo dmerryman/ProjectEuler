ac1989cb80fe765d84b78f1404db9e583bc061f7
David Merryman <DMerryman@nrvcs.org>
2021-11-17 14:32:05 -0500

    Solved

--- ProjectEulerTests/CountingSundaysTests.cs
+++ ProjectEulerTests/CountingSundaysTests.cs
@@ -11,5 +11,11 @@ namespace ProjectEulerTests
         {
             Assert.AreEqual(171, ProjectEuler.CountingSundays.FindCountingSundays(endYear: 2000));
         }
+
+        [TestMethod]
+        public void TestEasy()
+        {
+            Assert.AreEqual(171, ProjectEuler.CountingSundays.FindCountingSundaysEasy(endYear: 2000));
+        }
     }
 }
