3eb29d8e4662c09cc9c03feba56a9b304b013162
David Merryman <DMerryman@nrvcs.org>
2021-11-17 11:22:18 -0500

    Started IsItALeapYear

---
+++ ProjectEuler/CountingSundays.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class CountingSundays
+    {
+        public static int FindCountingSundays(int endYear)
+        {
+            int currDay = 1;    // 1 = Monday, 7 = Sunday
+            int currYear = 1900;
+            int numSundaysOnFirst = 0;
+            int[] normalDays = new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int[] leapYearDays = new int[] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            while (currYear <= 2000)
+            {
+                for (int month = 0; month < normalDays.Length; month++)
+                {
+
+                }
+                currYear++;
+            }
+
+            throw new NotImplementedException();
+            return numSundaysOnFirst;
+        }
+
+        private bool IsItALeapYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/CountingSundaysTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests
+{
+    [TestClass]
+    public class CountingSundaysTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.AreEqual(171, ProjectEuler.CountingSundays.FindCountingSundays(endYear: 2000));
+        }
+    }
+}
