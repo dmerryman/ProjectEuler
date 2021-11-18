ef68427023382243d94f6f4bbd3a282f7c44161e
David Merryman <DMerryman@nrvcs.org>
2021-11-18 15:25:55 -0500

    Started FindAmicableNumbers

---
+++ ProjectEuler/Problems21-30/AmicableNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems21_30
+{
+    public static class AmicableNumbers
+    {
+        public static int FindAmicableNumbers(int ceiling)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
