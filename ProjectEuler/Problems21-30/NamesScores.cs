0277b9458fe3cfb0c1540d2f73c78a96fa544038
David Merryman <DMerryman@nrvcs.org>
2021-11-18 16:50:56 -0500

    Started implementation

---
+++ ProjectEuler/Problems21-30/NamesScores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems21_30
+{
+    public static class NamesScores
+    {
+        public static long FindNamesScores()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems21-30/NamesScoresTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems21_30
+{
+    [TestClass]
+    public class NamesScoresTests
+    {
+        [TestMethod]
+        public void TestFindNamesScores()
+        {
+            Assert.AreEqual(871198282, ProjectEuler.Problems21_30.NamesScores.FindNamesScores());
+        }
+    }
+}
