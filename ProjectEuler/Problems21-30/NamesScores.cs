cdb15f395841c2a9b3d4703433f1a6048e6a11c8
David Merryman <DMerryman@nrvcs.org>
2021-11-18 16:53:28 -0500

    pseudocode added

--- ProjectEuler/Problems21-30/NamesScores.cs
+++ ProjectEuler/Problems21-30/NamesScores.cs
@@ -10,6 +10,11 @@ namespace ProjectEuler.Problems21_30
     {
         public static long FindNamesScores()
         {
+            // Read in names.txt into an ArrayList.
+            // Sort the arrayList alphabetically.
+            // Find the score for each name.
+                // Score is defined as the sum of the letter values (C = 3) multiplied by the name's position in the sorted list.
+            // Find and return the sum of the score of every name in the arraylist.
             throw new NotImplementedException();
         }
     }
