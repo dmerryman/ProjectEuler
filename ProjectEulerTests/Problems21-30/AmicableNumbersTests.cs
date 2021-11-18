dd0e086336573543c3366c756e4cc3ad668173d4
David Merryman <DMerryman@nrvcs.org>
2021-11-18 15:27:20 -0500

    started TestFindAmicableNumbers

---
+++ ProjectEulerTests/Problems21-30/AmicableNumbersTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems21_30
+{
+    [TestClass]
+    public class AmicableNumbersTests
+    {
+        [TestMethod]
+        public void TestFindAmicableNumbers()
+        {
+            Assert.AreEqual(31626, ProjectEuler.Problems21_30.AmicableNumbers.FindAmicableNumbers(10000));
+        }
+    }
+}
