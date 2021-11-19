42b8846d87f8b4556bcfa5c316f40fc90be0f311
David Merryman <DMerryman@nrvcs.org>
2021-11-19 11:35:40 -0500

    Added psuedocode to FindNonAbundantSums

--- ProjectEuler/Problems21-30/NonAbundantSums.cs
+++ ProjectEuler/Problems21-30/NonAbundantSums.cs
@@ -10,6 +10,14 @@ namespace ProjectEuler.Problems21_30
     {
         public static int FindNonAbundantSums()
         {
+            // An abundant number is a number if the sum of its proper divisors is greater than the number.
+            // All numbers greater than 28123 can be written as the sum of two abundant numbers.
+            
+            // Loop from 12 to 28123
+            // For each number, determine if it can be written as the sum of two abundant numbers.
+            // initialize sumOfNonAbundantNumbers to track the sum of nonabundant numbers.
+            // If the number cannot be written as the sum of two abundant numbers, add the number to SumOfNonAbundantNumbers
+            // return sumOfNonAbundantNumbers.
             throw new Exception();
         }
     }
