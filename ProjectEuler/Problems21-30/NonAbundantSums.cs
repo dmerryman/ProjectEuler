ae1155eaeadea7c96f02970940d4d7628bcfb7ea
David Merryman <DMerryman@nrvcs.org>
2021-11-19 11:30:46 -0500

    Started FindNonAbundantSums and a unit test

---
+++ ProjectEuler/Problems21-30/NonAbundantSums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems21_30
+{
+    public static class NonAbundantSums
+    {
+        public static int FindNonAbundantSums()
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems21-30/NonAbundantSumsTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems21_30
+{
+    [TestClass]
+    public class NonAbundantSumsTests
+    {
+        [TestMethod]
+        public void TestFindNonAbundantSums()
+        {
+            Assert.AreEqual(4179871, ProjectEuler.Problems21_30.NonAbundantSums.FindNonAbundantSums());
+        }
+    }
+}
