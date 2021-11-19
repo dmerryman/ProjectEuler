60dad819d61f23d99074a499ff60c5848ea0c8f1
David Merryman <DMerryman@nrvcs.org>
2021-11-19 10:50:39 -0500

    Implemented GetNameScore

--- ProjectEuler/Problems21-30/NamesScores.cs
+++ ProjectEuler/Problems21-30/NamesScores.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +16,29 @@ namespace ProjectEuler.Problems21_30
             // Find the score for each name.
                 // Score is defined as the sum of the letter values (C = 3) multiplied by the name's position in the sorted list.
             // Find and return the sum of the score of every name in the arraylist.
-            throw new NotImplementedException();
+            String[] names = SharedCode.InputHelper.ReadCSVInputNoQuotes(
+                path: "C:\\Users\\dmerryman\\Documents\\repos\\ProjectEuler\\ProjectEuler\\Resources\\p022_names.txt");
+            int sumOfScores = 0;
+            Array.Sort(names);
+            for (int i = 0; i < names.Length; i++)
+            {
+                sumOfScores += GetNameScore(name: names[i], position: i);
+            }
+
+            return sumOfScores;
+        }
+
+        private static int GetNameScore(String name, int position)
+        {
+            int score = 0;
+            char[] letters = name.ToCharArray();
+            foreach (char c in letters)
+            {
+                score += (int)c - 64;
+            }
+
+            score *= (position + 1);
+            return score;
         }
     }
 }
---
+++ ProjectEuler/SharedCode/InputHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.SharedCode
+{
+    public static class InputHelper
+    {
+        public static String[] ReadCSVInput(string path)
+        {
+            StreamReader reader = File.OpenText(path: path);
+            String str = reader.ReadLine();
+            string[] pieces = str.Split(',');
+            return pieces;
+        }
+
+        public static String[] ReadCSVInputNoQuotes(string path)
+        {
+            StreamReader reader = File.OpenText(path: path);
+            String str = reader.ReadLine();
+            str = str.Replace("\"", String.Empty);
+            string[] pieces = str.Split(',');
+            return pieces;
+        }
+    }
+}
