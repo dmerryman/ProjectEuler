edb1ebeed0450e5e0db81d16afaa2221d6393607
David Merryman <DMerryman@nrvcs.org>
2021-12-02 16:26:11 -0500

    Started FindQuadraticPrimes and a unit test

---
+++ ProjectEulerTests/Problems21-30/QuadraticPrimesTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems21_30
+{
+    [TestClass]
+    public class QuadraticPrimesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(-59231, ProjectEuler.Problems21_30.QuadraticPrimes.FindQuadraticPrimes(limit: 1000));
+        }
+    }
+}
