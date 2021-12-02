07ca3accdfb368e2ae8e47a27242d1403a2b2040
David Merryman <DMerryman@nrvcs.org>
2021-12-02 16:43:08 -0500

    Added pseudocode

--- ProjectEuler/Problems21-30/QuadraticPrimes.cs
+++ ProjectEuler/Problems21-30/QuadraticPrimes.cs
@@ -10,6 +10,18 @@ namespace ProjectEuler.Problems21_30
     {
         public static int FindQuadraticPrimes(int limit)
         {
+            // Initialize largestNumConsecutivePrimes = Int.MinValue.
+            // Initialize largestNumConsecutivePrimesA = Int.MinValue.
+            // Initialize largestNumConsecutivePrimesB = Int.MinValue.
+            // for (int i = -limit to limit)
+            // for (int j = -limit to limit)
+            // Initialize int n to 0.
+            // Initliaze numConsecutivePrimes = 0.
+            // Loop from n to infinity until f(x) is not prime
+            // If f(x) is prime, incrememnt numConsecutivePrimes.
+            // If f(x) is not prime, break out of the loop.
+            // if numConsecutivePrimes > largestNumConsecutivePrimes, store that value, and A and B.
+            // return largestNumConsecutivePrimesA * largestNumConsecutivePrimesB
             throw new NotImplementedException();
         }
     }
