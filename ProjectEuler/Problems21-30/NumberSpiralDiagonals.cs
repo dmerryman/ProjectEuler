1afa7c682202e85400b5116d78b6582d30e020c1
David Merryman <DMerryman@nrvcs.org>
2021-12-03 16:47:49 -0500

    Solved without spiral

--- ProjectEuler/Problems21-30/NumberSpiralDiagonals.cs
+++ ProjectEuler/Problems21-30/NumberSpiralDiagonals.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,14 +17,77 @@ namespace ProjectEuler.Problems21_30
             // start with [size / 2, size / 2]
             // Go right 1, down currSize / 2, left currSize - 1, up currSize - 1, right currSize - 1,
             // incrementing number as you go. For each spiral completion, add 2 to currSize, with it starting at 3.
-            int[,] spiral = new int[size, size];
+            int[,] spiral = GenerateSpiral(size: size);
 
             throw new NotImplementedException();
         }
 
+        public static int FindNumberSpiralDiagonalsNoSpiral(int size)
+        {
+            int currSum = 1;
+            int currNumber = 1;
+            int currInterval = 2;
+            while (currNumber < Math.Pow(x: size, y: 2))
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    currNumber += currInterval;
+                    currSum += currNumber;
+                }
+
+                currInterval += 2;
+            }
+
+            return currSum;
+        }
+
         private static int[,] GenerateSpiral(int size)
         {
-            throw new NotImplementedException();
+            int[,] spiral = new int [size, size];
+            int currNumber = 1;
+            int currCol = size / 2;
+            int currRow = size / 2;
+            spiral[currRow, currCol] = currNumber;
+            int currInterval = 3;
+            currNumber++;
+            while (currNumber < Math.Pow(x: size, y: 2))
+            {
+                currCol++;
+                spiral[currRow, currCol] = currNumber;
+                currNumber++;
+                Debug.WriteLine("{0}", currInterval / 2);
+                for (int i = 0; i <= currInterval / 2; i++)
+                {
+                    currRow++;
+                    spiral[currRow, currCol] = currNumber;
+                    currNumber++;
+                }
+
+                for (int i = 0; i < currInterval - 1; i++)
+                {
+                    currCol--;
+                    spiral[currRow, currCol] = currNumber;
+                    currNumber++;
+                }
+
+                for (int i = 0; i < currInterval - 1; i++)
+                {
+                    currRow--;
+                    spiral[currRow, currCol] = currNumber;
+                    currNumber++;
+                }
+
+                for (int i = 0; i < currInterval - 1; i++)
+                {
+                    currCol++;
+                    spiral[currRow, currCol] = currNumber;
+                    currNumber++;
+                }
+
+                currInterval += 2;
+            }
+
+            return spiral;
         }
     }
 }
--- ProjectEulerTests/Problems21-30/NumberSpiralDiagonals.cs
+++ ProjectEulerTests/Problems21-30/NumberSpiralDiagonals.cs
@@ -18,5 +18,19 @@ namespace ProjectEulerTests.Problems21_30
             Assert.AreEqual(669171001,
                 ProjectEuler.Problems21_30.NumberSpiralDiagonals.FindNumberSpiralDiagnoals(size: 1001));
         }
+
+        [TestMethod]
+        public void TestFindNumberSpiralDiagonalsNoSpiralSmall()
+        {
+            Assert.AreEqual(101,
+                ProjectEuler.Problems21_30.NumberSpiralDiagonals.FindNumberSpiralDiagonalsNoSpiral(size: 5));
+        }
+
+        [TestMethod]
+        public void TestFindNumberSpiralDiagonalsNoSpiralLarge()
+        {
+            Assert.AreEqual(669171001,
+                ProjectEuler.Problems21_30.NumberSpiralDiagonals.FindNumberSpiralDiagonalsNoSpiral(size: 1001));
+        }
     }
 }
