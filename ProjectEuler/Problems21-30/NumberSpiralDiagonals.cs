25f965f36cd09637fcf910371d46df154cc53500
David Merryman <DMerryman@nrvcs.org>
2021-12-03 15:54:47 -0500

    Started GenerateSpiral

---
+++ ProjectEuler/Problems21-30/NumberSpiralDiagonals.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems21_30
+{
+    public static class NumberSpiralDiagonals
+    {
+        public static int FindNumberSpiralDiagnoals(int size)
+        {
+            // Create a 2 dimensional array of [size, size].
+            // Initialize a variable for the current count, number = 1.
+            // start with [size / 2, size / 2]
+            // Go right 1, down currSize / 2, left currSize - 1, up currSize - 1, right currSize - 1,
+            // incrementing number as you go. For each spiral completion, add 2 to currSize, with it starting at 3.
+            int[,] spiral = new int[size, size];
+
+            throw new NotImplementedException();
+        }
+
+        private static int[,] GenerateSpiral(int size)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems21-30/NumberSpiralDiagonals.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems21_30
+{
+    [TestClass]
+    public class NumberSpiralDiagonals
+    {
+        [TestMethod]
+        public void TestFindNumberSpiralDiagonalsSmall()
+        {
+            Assert.AreEqual(101, ProjectEuler.Problems21_30.NumberSpiralDiagonals.FindNumberSpiralDiagnoals(size: 5));
+        }
+
+        [TestMethod]
+        public void TestFindNumberSpiralDiagonalsLarge()
+        {
+            Assert.AreEqual(669171001,
+                ProjectEuler.Problems21_30.NumberSpiralDiagonals.FindNumberSpiralDiagnoals(size: 1001));
+        }
+    }
+}
