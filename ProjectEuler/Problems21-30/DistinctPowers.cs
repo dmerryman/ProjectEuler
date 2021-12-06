74f39aa23470380a4066be81addb138745100131
David Merryman <DMerryman@nrvcs.org>
2021-12-06 16:04:50 -0500

    Started FindDistinctPowers and set up unit tests

---
+++ ProjectEuler/Problems21-30/DistinctPowers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems21_30
+{
+    public static class DistinctPowers
+    {
+        public static int FindDistinctPowers(int limit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems21-30/DistinctPowersTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems21_30
+{
+    [TestClass]
+    public class DistinctPowersTests
+    {
+        [TestMethod]
+        public void TestFindDistinctPowersSmall()
+        {
+            Assert.AreEqual(15, ProjectEuler.Problems21_30.DistinctPowers.FindDistinctPowers(limit: 5));
+        }
+
+        [TestMethod]
+        public void TestFindDistinctPowersLarge()
+        {
+            Assert.AreEqual(9183, ProjectEuler.Problems21_30.DistinctPowers.FindDistinctPowers(limit: 100));
+        }
+    }
+}
