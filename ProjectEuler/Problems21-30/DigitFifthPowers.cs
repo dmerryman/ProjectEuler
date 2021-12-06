95ee021f0ff9a8f7fc83bb7da53aec712348b80c
David Merryman <DMerryman@nrvcs.org>
2021-12-06 16:51:07 -0500

    added psuedocode

--- ProjectEuler/Problems21-30/DigitFifthPowers.cs
+++ ProjectEuler/Problems21-30/DigitFifthPowers.cs
@@ -10,6 +10,12 @@ namespace ProjectEuler.Problems21_30
     {
         public static int FindDigitFifthPowers()
         {
+            // Initialize sumOfDigitFifthPowers to 0.
+            // Loop for testValue from 32 to 354294
+            // for each testValue, get the individual digits, and check to see if the sum of each
+            // individual digit raised to the power of 5 is equal to testValue.
+            // If it is, add that number to sumOfDigitFifthPowers.
+            // return sumOfDigitFifthPowers.
             throw new NotImplementedException();
         }
     }
