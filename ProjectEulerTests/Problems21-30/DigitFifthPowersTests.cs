ca6fbb029ddf9aec61fcf407153e9fce7619b6bf
David Merryman <DMerryman@nrvcs.org>
2021-12-06 16:48:18 -0500

    Started FindDigitFifthPowers and unit test

---
+++ ProjectEulerTests/Problems21-30/DigitFifthPowersTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems21_30
+{
+    [TestClass]
+    public class DigitFifthPowersTests
+    {
+        [TestMethod]
+        public void TestFindDigitFifthPowers()
+        {
+            Assert.AreEqual(443839, ProjectEuler.Problems21_30.DigitFifthPowers.FindDigitFifthPowers());
+        }
+    }
+}
