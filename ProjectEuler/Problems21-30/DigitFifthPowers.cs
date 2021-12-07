2154cf7e3c4a22d9ca533456d2165b5e627bef92
David Merryman <DMerryman@nrvcs.org>
2021-12-07 13:31:52 -0500

    solved with a slightly more efficient method

--- ProjectEuler/Problems21-30/DigitFifthPowers.cs
+++ ProjectEuler/Problems21-30/DigitFifthPowers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,88 @@ namespace ProjectEuler.Problems21_30
             int sumOfDigitFifthPowers = 0;
             for (int testValue = 32; testValue <= 354294; testValue++)
             {
+                if (testValue == 4150)
+                {
+                    int pause = 1;
+                }
                 List<int> digits = GetDigits(testValue: testValue);
+                if (CanBeWrittenAsSumOfFifthPowers(testValue: testValue, digits: digits, power: 5))
+                {
+                    sumOfDigitFifthPowers += testValue;
+                }
             }
-            throw new NotImplementedException();
+
+            return sumOfDigitFifthPowers;
+        }
+
+        public static int FindDigitFifthPowersBetter(int power)
+        {
+            Dictionary<int, int> powersDictionary = FindPowerDigits(power: power);
+            int upperLimit = GetUpperLimit(power: power);
+            int sumOfDigitFifthPowers = 0;
+            for (int testValue = 121; testValue <= upperLimit; testValue++)
+            {
+                if (testValue == 4150)
+                {
+                    int pause = 1;
+                }
+                List<int> digits = GetDigits(testValue: testValue);
+                if (CanBeWrittenAsSumOfFifthPowersBetter(testValue: testValue, digits: digits,
+                        powersDictionary: powersDictionary))
+                {
+                    sumOfDigitFifthPowers += testValue;
+                }
+            }
+
+            return sumOfDigitFifthPowers;
+        }
+
+        private static int GetUpperLimit(int power)
+        {
+            int numDigits = 1;
+            bool increased = true;
+            int newSum = 0;
+            while (increased)
+            {
+                newSum = 0;
+                for (int i = 1; i <= numDigits; i++)
+                {
+                    newSum += (int)Math.Pow(x: 9, y: power);
+                    int newNumDigits = newSum.ToString().Length;
+                    if (newNumDigits == numDigits)
+                    {
+                        increased = false;
+                    }
+
+                    numDigits = newNumDigits;
+                }
+            }
+
+            return newSum;
+        }
+
+        private static bool CanBeWrittenAsSumOfFifthPowersBetter(int testValue, List<int> digits,
+            Dictionary<int, int> powersDictionary)
+        {
+            int currSum = 0;
+            foreach (int i in digits)
+            {
+                currSum += powersDictionary[i];
+            }
+
+            return currSum == testValue;
+        }
+
+        private static Dictionary<int, int> FindPowerDigits(int power)
+        {
+            Dictionary<int, int> powersDictionary = new Dictionary<int, int>();
+            for (int i = 2; i <= 9; i++)
+            {
+                powersDictionary.Add(key: i, value: (int)Math.Pow(x: i, y: power));
+            }
+            powersDictionary.Add(key: 0, value: 0);
+            powersDictionary.Add(key: 1, value: 1);
+            return powersDictionary;
         }
 
         private static List<int> GetDigits(int testValue)
@@ -40,9 +120,16 @@ namespace ProjectEuler.Problems21_30
             return digits;
         }
 
-        private static bool CanBeWrittenAsSumOfFifthPowers(int testValue, List<int> digits)
+        private static bool CanBeWrittenAsSumOfFifthPowers(int testValue, List<int> digits, int power)
         {
-            throw new NotImplementedException();
+            int currSum = 0;
+            foreach (int i in digits)
+            {
+                currSum += (int)Math.Pow(x: i, y: power);
+
+            }
+
+            return currSum == testValue;
         }
     }
 }
--- ProjectEulerTests/Problems21-30/DigitFifthPowersTests.cs
+++ ProjectEulerTests/Problems21-30/DigitFifthPowersTests.cs
@@ -7,9 +7,15 @@ namespace ProjectEulerTests.Problems21_30
     public class DigitFifthPowersTests
     {
         [TestMethod]
-        public void TestFindDigitFifthPowers()
+        public void TestFindDigitFifthPowersBasic()
         {
             Assert.AreEqual(443839, ProjectEuler.Problems21_30.DigitFifthPowers.FindDigitFifthPowersBasic());
+        }
+
+        [TestMethod]
+        public void TestFindDigitFifthPowersBetter()
+        {
+            Assert.AreEqual(443839, ProjectEuler.Problems21_30.DigitFifthPowers.FindDigitFifthPowersBetter(power: 5));
         }
     }
 }
