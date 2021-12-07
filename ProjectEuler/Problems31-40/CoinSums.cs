034896f3f229a14b64e4e8d0d294b8741e368ea5
David Merryman <DMerryman@nrvcs.org>
2021-12-07 13:37:49 -0500

    Set up FindCoinSums and unit test

---
+++ ProjectEuler/Problems31-40/CoinSums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems31_40
+{
+    public static class CoinSums
+    {
+        public static int FindCoinSums(int pounds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems31-40/CoinSumsTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems31_40
+{
+    [TestClass]
+    public class CoinSumsTests
+    {
+        [TestMethod]
+        public void TestFindCoinSums()
+        {
+            Assert.AreEqual(1, ProjectEuler.Problems31_40.CoinSums.FindCoinSums(pounds: 2));
+        }
+    }
+}
