fcb36a38f421edd840345e8eaa147ed5b940ddac
David Merryman <DMerryman@nrvcs.org>
2021-12-08 13:38:34 -0500

    Added pseudocode

--- ProjectEuler/Problems31-40/PandigitalProducts.cs
+++ ProjectEuler/Problems31-40/PandigitalProducts.cs
@@ -10,6 +10,14 @@ namespace ProjectEuler.Problems31_40
     {
         public static int FindPandigitalProducts()
         {
+            // int currSum to hold sum of pandigital products.
+            // for int index = lower limit to upper limit
+                // Calculate the factors of index
+                // Loop through each combination of the factors of index.
+                // Combine multiplicand, multiplier, and product, and check to see if its
+                // pandigital.
+                    // If it is, add index to currSum and break.
+            // return currSum
             throw new NotImplementedException();
         }
     }
