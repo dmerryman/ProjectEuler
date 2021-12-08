00edb750e0e0fb3c0c9c4f74fb17913c6f143bd0
David Merryman <DMerryman@nrvcs.org>
2021-12-08 13:36:23 -0500

    Started FindPandigitalProducts

---
+++ ProjectEuler/Problems31-40/PandigitalProducts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems31_40
+{
+    public static class PandigitalProducts
+    {
+        public static int FindPandigitalProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems31-40/PandigitalProducts.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems31_40
+{
+    [TestClass]
+    public class PandigitalProducts
+    {
+        [TestMethod]
+        public void TestFindPandigitalProducts()
+        {
+            Assert.AreEqual(1, ProjectEuler.Problems31_40.PandigitalProducts.FindPandigitalProducts());
+        }
+    }
+}
