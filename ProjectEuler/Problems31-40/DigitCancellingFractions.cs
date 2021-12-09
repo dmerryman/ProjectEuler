d3d1fbe9b4ea8d4fd93b37eee7a063acb374583d
David Merryman <DMerryman@nrvcs.org>
2021-12-09 11:03:11 -0500

    Started FindDigitCancellingFractions and unit test

---
+++ ProjectEuler/Problems31-40/DigitCancellingFractions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems31_40
+{
+    public static class DigitCancellingFractions
+    {
+        public static int FindDigitCancellingFractions()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
