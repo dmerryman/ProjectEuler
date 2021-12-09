a97cf295c87816ffe4e380416df9432efde886e2
David Merryman <DMerryman@nrvcs.org>
2021-12-09 16:37:09 -0500

    Started FindDigitFactorials and Unit Test

---
+++ ProjectEuler/Problems31-40/DigitFactorials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems31_40
+{
+    public static class DigitFactorials
+    {
+        public static int FindDigitFactorials()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems31-40/DigitFactorialsTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems31_40
+{
+    [TestClass]
+    public class DigitFactorialsTests
+    {
+        [TestMethod]
+        public void TestFindDigitFactorials()
+        {
+            Assert.AreEqual(1, ProjectEuler.Problems31_40.DigitFactorials.FindDigitFactorials());
+        }
+    }
+}
