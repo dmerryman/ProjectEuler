e5dcfd140443d8e51daec7220ea32d2912d9b1b7
David Merryman <DMerryman@nrvcs.org>
2021-12-10 15:40:04 -0500

    added pseudocode

---
+++ ProjectEuler/Problems31-40/CircularPrimes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems31_40
+{
+    public static class CircularPrimes
+    {
+        public static int FindCircularPrimes(int limit)
+        {
+            // int numOfCircularPrimes = 0 to track number of circular primes.
+            // Loop for int testValue from 2 to limit - 1
+            // For each testValue, get the permutations of each value.
+                // Loop through each permutation
+                // if all permutations are prime, then increment numOfCircularPrimes by one.
+            // return numOfCircularPrimes
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems31-40/CircularPrimesTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ProjectEulerTests.Problems31_40
+{
+    [TestClass]
+    public class CircularPrimesTests
+    {
+        [TestMethod]
+        public void TestFindCircularPrimesSmall()
+        {
+            Assert.AreEqual(13, ProjectEuler.Problems31_40.CircularPrimes.FindCircularPrimes(limit: 100));
+        }
+
+        [TestMethod]
+        public void TestFindCircularPrimesLarge()
+        {
+            Assert.AreEqual(13, ProjectEuler.Problems31_40.CircularPrimes.FindCircularPrimes(limit: 1000000));
+        }
+    }
+}
