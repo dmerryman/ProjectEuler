9d7010a2837a24669a7c02596619175637e5a8bd
David Merryman <DMerryman@nrvcs.org>
2021-12-13 17:00:15 -0500

    Started FindTrunctablePrimes and unit test

---
+++ ProjectEuler/Problems31-40/TrunctablePrimes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems31_40
+{
+    public static class TrunctablePrimes
+    {
+        public static int FindTrunctablePrimes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
