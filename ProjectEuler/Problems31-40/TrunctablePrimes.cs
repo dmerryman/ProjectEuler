a5a65a3fa1913e1f9219469357e68d01c98dfb51
David Merryman <DMerryman@nrvcs.org>
2021-12-14 10:15:44 -0500

    Added psuedocode

--- ProjectEuler/Problems31-40/TrunctablePrimes.cs
+++ ProjectEuler/Problems31-40/TrunctablePrimes.cs
@@ -10,6 +10,14 @@ namespace ProjectEuler.Problems31_40
     {
         public static int FindTrunctablePrimes()
         {
+            // int numTrunctablePrimes = 0 to track the number of trunctable primes.
+            // int sumTrunctablePrimes = 0 to track the sum of trunctable primes.
+            // Loop until numTrunctablePrimes == 11
+                // Check if a number is prime.
+                    // if it is, get a list of each possible variation of the number, and check to see if they're prime.
+                    // if they all are, increment numTrunctablePrimes by one, and add the number to 
+                    // sumTrunctablePrimes.
+            // return sumTrunctablePrimes.
             throw new NotImplementedException();
         }
     }
