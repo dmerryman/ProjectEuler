b14cfff71ebd1ea14741675793ccd1b7efce74bc
David Merryman <DMerryman@nrvcs.org>
2021-12-14 15:19:17 -0500

    added pseudocode

--- ProjectEuler/Problems31-40/PandigitalMultiples.cs
+++ ProjectEuler/Problems31-40/PandigitalMultiples.cs
@@ -10,6 +10,12 @@ namespace ProjectEuler.Problems31_40
     {
         static public int FindPandigitalMultiples()
         {
+            // int largestPandigitalMultiple = int32.minvalue to track the largest pandigital multiple.
+            // loop for int testValue from 1 to 98765432
+            // Determeine if testValue is a pandigital multiple.
+                // If it is, check to see if it's larger than largestPandigitalMultiple
+                    // if it is, set largestPandigitalMultiple to that value.
+            // return largestPandigitalMultiple
             throw new NotImplementedException();
         }
 
