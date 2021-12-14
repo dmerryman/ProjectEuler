fe967f87e9cf459dbe7457b2aab6135c93fab8ed
David Merryman <DMerryman@nrvcs.org>
2021-12-14 15:15:05 -0500

    started FindPandigitalMultiples and unit test

---
+++ ProjectEuler/Problems31-40/PandigitalMultiples.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems31_40
+{
+    static public class PandigitalMultiples
+    {
+        static public int FindPandigitalMultiples()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
