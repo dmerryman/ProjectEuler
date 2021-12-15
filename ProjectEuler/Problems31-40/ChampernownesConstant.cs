ed5b4224db7a1e5c2fae29b420b741ded032461e
David Merryman <DMerryman@nrvcs.org>
2021-12-15 16:03:48 -0500

    added pseudocode

--- ProjectEuler/Problems31-40/ChampernownesConstant.cs
+++ ProjectEuler/Problems31-40/ChampernownesConstant.cs
@@ -10,6 +10,14 @@ namespace ProjectEuler.Problems31_40
     {
         public static int FindChampernownesConstant()
         {
+            // int digitCount = 0 to track number of digits.
+            // int currNum = 0 to track the current number.
+            // int multipleValue = 1 to track the multiple value to be returned.
+                // Loop while digitCount <= 1000000
+                    // increment currNum by one.
+                    // Check to see if this value contains one of the decimal places we need to track.
+                        // if it does, multiple multipleValue by that digit.
+            // return multipleValue
             throw new NotImplementedException();
         }
     }
