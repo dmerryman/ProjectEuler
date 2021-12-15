cce9228b95b91d4e77fe436634a6002ac4f4c5a1
David Merryman <DMerryman@nrvcs.org>
2021-12-15 09:49:08 -0500

    Started FindIntegerRightTriangles and unit test

---
+++ ProjectEuler/Problems31-40/IntegerRightTriangles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems31_40
+{
+    public static class IntegerRightTriangles
+    {
+        public static int FindIntegerRightTriangles(int perimiterLimit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
