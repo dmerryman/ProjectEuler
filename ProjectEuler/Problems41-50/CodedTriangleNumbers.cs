ea1bac9866c79f758b6fad7a852fed994ca27a13
David Merryman <DMerryman@nrvcs.org>
2021-12-16 15:57:44 -0500

    Added pseudocode

--- ProjectEuler/Problems41-50/CodedTriangleNumbers.cs
+++ ProjectEuler/Problems41-50/CodedTriangleNumbers.cs
@@ -10,6 +10,12 @@ namespace ProjectEuler.Problems41_50
     {
         public static int FindCodedTriangleNumbers()
         {
+            // int numberOfTriangleWords = 0 to track number of triangle words.
+            // Read the file into memory.
+            // Loop through each word in the file, and determine its score.
+                // for each score, check to see if it is a triangle number.
+                    // if it is, then increment numberOfTriangleWords by 1.
+            // return number of triangle words.
             throw new NotImplementedException();
         }
     }
