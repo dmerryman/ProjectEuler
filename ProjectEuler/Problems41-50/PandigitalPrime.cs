9cb2d5f46bac10a867787af755ce1f3b702ce22e
David Merryman <DMerryman@nrvcs.org>
2021-12-16 11:38:38 -0500

    added pseudocode

--- ProjectEuler/Problems41-50/PandigitalPrime.cs
+++ ProjectEuler/Problems41-50/PandigitalPrime.cs
@@ -10,6 +10,13 @@ namespace ProjectEuler.Problems41_50
     {
         public static int FindPandigitalPrime()
         {
+            // int largestPandigitalPrime = int32.minvalue
+            // Loop for int testValue = 2 to 987654321
+                // Check to see if testValue is prime.
+                    // if it is, check to see if it is also pandigital.
+                        // if it is, check to see if it is larger than largestPandigitalPrime
+                            // if it is, then set largestPandigitalPrime = testValue
+            // return testValue
             throw new NotImplementedException();
         }
     }
