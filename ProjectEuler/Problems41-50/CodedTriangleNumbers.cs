7b0f1cef7e4124431627727a8ecbd0655174e42c
David Merryman <DMerryman@nrvcs.org>
2021-12-16 15:43:07 -0500

    Started FindCodedTriangleNumbers and unit test

---
+++ ProjectEuler/Problems41-50/CodedTriangleNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems41_50
+{
+    public static class CodedTriangleNumbers
+    {
+        public static int FindCodedTriangleNumbers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
