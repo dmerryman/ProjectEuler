4bc0ee37a1c8b279b7a6c15148dc3b799e38536e
David Merryman <DMerryman@nrvcs.org>
2021-12-17 09:11:00 -0500

    Started FindSubstringDivisibility and unit test

---
+++ ProjectEuler/Problems41-50/SubstringDivisibility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems41_50
+{
+    public static class SubstringDivisibility
+    {
+        public static int FindSubstringDivisibility()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
