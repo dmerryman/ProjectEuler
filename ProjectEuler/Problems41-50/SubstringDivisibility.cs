33c8c2f2a62b847ee001d44890802d3db90a47bc
David Merryman <DMerryman@nrvcs.org>
2021-12-17 09:21:13 -0500

    added psuedocode and started IsItSubstringDivisible

--- ProjectEuler/Problems41-50/SubstringDivisibility.cs
+++ ProjectEuler/Problems41-50/SubstringDivisibility.cs
@@ -10,6 +10,17 @@ namespace ProjectEuler.Problems41_50
     {
         public static int FindSubstringDivisibility()
         {
+            // int sumOfSubstringDivisibleNumbers = 0 to track the sum of substring divisible numbers
+            // Loop for int testValue from 123456789 to 987654321
+                // Check to see if the number is pandigital.
+                    // if it is, then check to see if its substring divisible.
+                        // if is, then add that value to sumOfSubstringDivisibleNumbers
+            // return sumOfSubstringDivisibleNumbers
+            throw new NotImplementedException();
+        }
+
+        public static bool IsItSubstringDivisible(int testValue)
+        {
             throw new NotImplementedException();
         }
     }
