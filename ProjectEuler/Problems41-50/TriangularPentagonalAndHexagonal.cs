6118a1a43b8957eecc5a2e8a56c9daa5b4eec16f
David Merryman <DMerryman@nrvcs.org>
2021-12-21 16:58:31 -0500

    done for the day

---
+++ ProjectEuler/Problems41-50/TriangularPentagonalAndHexagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems41_50
+{
+    public static class TriangularPentagonalAndHexagonal
+    {
+        public static int FindTriangularPentagonalAndHexagonal()
+        {
+            bool[] sieve = new bool[100000];
+            for (int i = 1; i < sieve.Length; i++)
+            {
+
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
