9d198a652d16ab0229a4cb32dcf85f18a53371c2
David Merryman <DMerryman@nrvcs.org>
2021-12-29 13:21:00 -0500

    implemented GetNumberOfDistinctPrimeFactors

---
+++ ProjectEuler/Problems41-50/DistinctPrimesFactors.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems41_50
+{
+    public static class DistinctPrimesFactors
+    {
+        public static int FindDistinctPrimesFactors(int numConsecutive)
+        {
+            // int currNumConsecutive = 0 to track number of consecutive numbers
+            // int currFirstNum = Int32.minvalue to track the first number in the set of consecutive numbers.
+            // int currNum = 1 to track the current test number.
+                // Loop while currNumConsecutive < numConsecutive
+                    // Check to see if this number has numConsecutive distinct Prime factors
+                        // if so, incrememnt currNumConsecutive by one, and set currFirstNum to currNum if currNumConsecutive = 0.
+                        // if not, set currNumConsecutive to 0.
+                    // increment currNum.
+            int currNumConsecutive = 0;
+            int currFirstNum = Int32.MinValue;
+            int currNum = 1;
+            while (currNumConsecutive < numConsecutive)
+            {
+
+            }
+            throw new NotImplementedException();
+        }
+
+        public static int GetNumberOfDistinctPrimeFactors(int number)
+        {
+            Dictionary<int, int> distinctPrimeFactorsDictionary = new Dictionary<int, int>();
+            int loopThrough = number;
+            for (int i = 2; i <= loopThrough / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    distinctPrimeFactorsDictionary.Add(key: i, value: 0);
+                    while (number % i == 0)
+                    {
+                        number /= i;
+                        distinctPrimeFactorsDictionary[i]++;
+                    }
+                }
+            }
+
+            return distinctPrimeFactorsDictionary.Keys.Count;
+        }
+    }
+}
---
+++ ProjectEulerTests/Problems41-50/DistinctPrimesFactorsTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEulerTests.Problems41_50
+{
+    /// <summary>
+    /// Summary description for DistinctPrimesFactorsTests
+    /// </summary>
+    [TestClass]
+    public class DistinctPrimesFactorsTests
+    {
+        public DistinctPrimesFactorsTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestFindDistinctPrimesFactors()
+        {
+            Assert.AreEqual(14,
+                ProjectEuler.Problems41_50.DistinctPrimesFactors.FindDistinctPrimesFactors(numConsecutive: 2));
+            Assert.AreEqual(14,
+                ProjectEuler.Problems41_50.DistinctPrimesFactors.FindDistinctPrimesFactors(numConsecutive: 2));
+        }
+
+        [TestMethod]
+        public void TestGetNumberOfDistinctPrimeFactors()
+        {
+            Assert.AreEqual(2,
+                ProjectEuler.Problems41_50.DistinctPrimesFactors.GetNumberOfDistinctPrimeFactors(number: 14));
+            Assert.AreEqual(2,
+                ProjectEuler.Problems41_50.DistinctPrimesFactors.GetNumberOfDistinctPrimeFactors(number: 15));
+            Assert.AreEqual(3,
+                ProjectEuler.Problems41_50.DistinctPrimesFactors.GetNumberOfDistinctPrimeFactors(number: 644));
+        }
+    }
+}
