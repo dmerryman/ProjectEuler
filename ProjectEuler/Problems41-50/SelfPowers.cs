3dd7d28f93d3ad34b6e857567cd491110f4953e5
David Merryman <DMerryman@nrvcs.org>
2021-12-29 15:09:19 -0500

    solved

---
+++ ProjectEuler/Problems41-50/SelfPowers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems41_50
+{
+    public static class SelfPowers
+    {
+        public static long FindSelfPowers(int limit)
+        {
+            BigInteger currentValue = 0;
+            for (int i = 1; i <= limit; i++)
+            {
+                currentValue = BigInteger.Add(left: BigInteger.Pow(value: i, exponent: i), right: currentValue);
+            }
+
+            return (long)(currentValue % new BigInteger(10000000000));
+        }
+    }
+}
