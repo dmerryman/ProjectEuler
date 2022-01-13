10e4f6bdfbb439ca45e4c5e8ffb5ae634e2460fd
David Merryman <DMerryman@nrvcs.org>
2022-01-13 16:59:56 -0500

    end of the day

---
+++ ProjectEuler/Problems51-60/CombinatoricSelections.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems51_60
+{
+    public static class CombinatoricSelections
+    {
+        public static int FindCombinatoricSelections()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
