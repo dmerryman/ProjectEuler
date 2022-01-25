747314d0b7b30dd5c2c0ac33ade1a8b2aa691783
David Merryman <DMerryman@nrvcs.org>
2022-01-25 14:30:44 -0500

    added pseudocode

--- ProjectEuler/Problems51-60/LychrelNumbers.cs
+++ ProjectEuler/Problems51-60/LychrelNumbers.cs
@@ -10,6 +10,12 @@ namespace ProjectEuler.Problems51_60
     {
         public static int FindLychrelNumbers(int numIterations, int limit)
         {
+            // int numLychrelNumbers = 0 to track the number of lychrel numbers.
+            // for int currNum = 1 to 9999
+                // Determine if currNum can be made into a Lychrel Number in less than numIterations iterations.
+                    // if it can be, incrememt numLychrelNumbers by one.
+            // return numLychrelNumbers.
+                
             throw new NotImplementedException();
         }
     }
