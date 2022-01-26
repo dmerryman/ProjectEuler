957f8a5ec953a9cfde68c1157f89aa593555c217
David Merryman <DMerryman@nrvcs.org>
2022-01-26 10:52:05 -0500

    set up unit test

---
+++ ProjectEuler/Problems51-60/PowerfulDigitSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems51_60
+{
+    public static class PowerfulDigitSum
+    {
+        public static int FindPowerfulDigitSum(int limit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
