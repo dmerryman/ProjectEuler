7a3dcb41f05d1ca460bf6aca1362a72bb2cb22e5
David Merryman <DMerryman@nrvcs.org>
2022-01-26 10:54:48 -0500

    wrote pseudocode

--- ProjectEuler/Problems51-60/PowerfulDigitSum.cs
+++ ProjectEuler/Problems51-60/PowerfulDigitSum.cs
@@ -10,6 +10,14 @@ namespace ProjectEuler.Problems51_60
     {
         public static int FindPowerfulDigitSum(int limit)
         {
+            //int maximumDigitalSum = -1 to track the maximumDigitalSum
+            //for int a = 1 to limit - 1
+                // for int b = 1 to limit - 1
+                    // Calculate a^b
+                    // Calculate the sum of digits in a^b
+                    // Check to see if the sum of digits in a^b > maximumDigitalSum
+                        // If it is, then set maximumDigitalSum to that result.
+            // return maximumDigitalSum
             throw new NotImplementedException();
         }
     }
