74bcba3dfdca0e4da2729382228ee0c55c8bc156
David Merryman <DMerryman@nrvcs.org>
2022-01-27 14:01:42 -0500

    wrote pseudocode

---
+++ ProjectEuler/Problems51-60/SpiralPrimes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.Problems51_60
+{
+    public static class SpiralPrimes
+    {
+        public static int FindSpiralPrimes(int percentageLimit)
+        {
+            // decimal percentage = 1.00 to track percentage of prime numbers in spiral.
+            // int length = 1 to track the length of the side of the spiral.
+            // int numPrimes = 0 to track number of prime numbers in spiral.
+            // int numValues = 0 to track number of values.
+                // Loop until percentage < percentageLimit / 100
+                    // Iterate through the spiral by adding length + 1 4 times and checking each number to see if its a prime.
+                        // For every prime, increment numPrimes by 1.
+                        // For every number, 
+                    // Calculate the percentage of primes.
+                    // Increment length by 2.
+            // return length
+            throw new NotImplementedException();
+        }
+    }
+}
